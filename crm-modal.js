8a692f0f61d506e89baebf8d8d0b514094a2512d
Dennys <keta548@vivaldi.net>
2022-12-11 17:22:51 +0400

    поправил верстку добавил коснстатнту -id

--- crm-modal.js
+++ crm-modal.js
@@ -1,4 +1,5 @@
 const modalWindowTitle = document.querySelector('.crm-modal-window__title');
+const modalWindowProductId = document.querySelector('.crm-modal-window__product-id');
 const modalWindowCloseBtn = document.querySelector('.crm-modal-window--close');
 const modalWindowForm = document.querySelector('.crm-modal-window__form');
 const modalWindowCheckbox = document.querySelector('.crm-modal-window--checkbox-style');
